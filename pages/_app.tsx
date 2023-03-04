8db19fa2c346cebc0a270b1dc5bb0a0de34bd46e
haitham qudaih <haithamqdaih@gmail.com>
2023-03-04 20:56:14 +0400

    initial commit with superplate

---
+++ pages/_app.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { AppProps } from "next/app";
+import { Refine } from "@pankod/refine-core";
+import {
+  notificationProvider,
+  RefineSnackbarProvider,
+  CssBaseline,
+  GlobalStyles,
+  ReadyPage,
+  ErrorComponent,
+} from "@pankod/refine-mui";
+import routerProvider from "@pankod/refine-nextjs-router";
+import dataProvider from "@pankod/refine-simple-rest";
+import { ColorModeContextProvider } from "@contexts";
+import { Title, Sider, Layout, Header } from "@components/layout";
+
+const API_URL = "https://api.fake-rest.refine.dev";
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <ColorModeContextProvider>
+      <CssBaseline />
+      <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
+      <RefineSnackbarProvider>
+        <Refine
+          routerProvider={routerProvider}
+          dataProvider={dataProvider(API_URL)}
+          notificationProvider={notificationProvider}
+          ReadyPage={ReadyPage}
+          catchAll={<ErrorComponent />}
+          Title={Title}
+          Sider={Sider}
+          Layout={Layout}
+          Header={Header}
+        >
+          <Component {...pageProps} />
+        </Refine>
+      </RefineSnackbarProvider>
+    </ColorModeContextProvider>
+  );
+}
+
+export default MyApp;
